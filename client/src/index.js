7f7cccd004adc23f86a6428123a8df8f65fc20d9
Cody <codybarker.or@gmail.com>
2023-09-04 13:00:37 -0700

    updating to react v 18

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from "react-redux";
 import store from "./store"
 
-//wrapping with Provider returns ×
+//wrapping with Provider returns
 //TypeError: Cannot read properties of null (reading 'useMemo')
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+      <Provider store={store}>
+          <App />
+      </Provider>
 );
 
