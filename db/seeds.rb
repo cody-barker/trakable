5b3cb05e12678712e96ca1e6f8ff8f566cdc2a4e
Cody <codybarker.or@gmail.com>
2023-09-04 09:32:08 -0700

    created db, user resource, first migration, and user seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create(
+    first_name: "Cody",
+    last_name: "Barker",
+    title: "Project Manager",
+    password: "test",
+    email: "codybarker.or@gmail.com"
+)
+
+user2 = User.create(
+    first_name: "Kelli"
+    last_name: "Radwanski",
+    title: "UX Lead"
+    password: "test"
+    email: "kelli.radwanski@gmail.com"
+)
