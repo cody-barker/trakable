4203fbe0bdf6caa1c8c07d81211f89b03152db3d
Cody <codybarker.or@gmail.com>
2023-09-04 09:37:41 -0700

    including has_secure_password and seeding the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-user1 = User.create(
+user1 = User.create!(
     first_name: "Cody",
     last_name: "Barker",
     title: "Project Manager",
@@ -6,10 +6,10 @@ user1 = User.create(
     email: "codybarker.or@gmail.com"
 )
 
-user2 = User.create(
-    first_name: "Kelli"
+user2 = User.create!(
+    first_name: "Kelli",
     last_name: "Radwanski",
-    title: "UX Lead"
-    password: "test"
+    title: "UX Lead",
+    password: "test",
     email: "kelli.radwanski@gmail.com"
 )
