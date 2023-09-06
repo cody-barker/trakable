d425f3aa8e1837b843fd70b84d967792dccaada3
Cody <codybarker.or@gmail.com>
2023-09-06 15:41:23 -0700

    requests to users index returns associated tasks

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,4 +1,4 @@
 class TaskSerializer < ActiveModel::Serializer
-  attributes :id, :name, :due_date, :description, :user_id, :peroject_id, :team_id
+  attributes :id, :name, :due_date, :description, :user_id, :project_id, :team_id
   belongs_to :user
 end
