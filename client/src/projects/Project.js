51fe93fe5f5df2cdf188d018853d04cc05c076b4
Cody <codybarker.or@gmail.com>
2023-09-07 16:31:19 -0700

    users can now see all of their tasks by project

--- client/src/projects/Project.js
+++ client/src/projects/Project.js
@@ -1,7 +1,22 @@
+//Render all tasks for a single project
+import {useParams} from 'react-router-dom'
+import {useSelector} from 'react-redux'
+import TaskCard from '../tasks/TaskCard'
+
 function Project() {
+
+    let {id} = useParams()
+    id = parseInt(id)
+    const currentUser = useSelector((state) => state.users.currentUser)
+    const project = currentUser.projects.find((project) => project.id === id)
+    const tasks = currentUser.tasks.filter((task) => task.project_id === id)
+    const taskComps = tasks.map((task) => <TaskCard key={task.id} task={task}/>)
+    console.log(currentUser)
+
     return(
        <div>
-        Project
+        {project.name}
+        {taskComps}
        </div>
     )
 }
