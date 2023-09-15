6d83e55a7cad844f74e36c2485d11f369f11f24e
Cody <codybarker.or@gmail.com>
2023-09-15 15:08:18 -0700

    removing tasks from users serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :title, :email, :projects, :teams
   
-  has_many :tasks
+  # has_many :tasks
   has_many :projects
   has_many :teams
 
