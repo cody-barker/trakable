f426ca3e51c3b18850b77ff1790183f0aa49153b
Cody <codybarker.or@gmail.com>
2023-09-15 15:20:46 -0700

    cleanup

--- client/src/teams/Team.js
+++ client/src/teams/Team.js
@@ -6,11 +6,6 @@ function Team() {
 
     let {id} = useParams();
     id = parseInt(id)
-    // const allTeams = useSelector((state) => state.teams.entities)
-    // const currentUser = useSelector((state) => state.users.currentUser)
-    // const team = allTeams.find((team) => team.id === id)
-    // const teamTasks = currentUser.tasks.filter((task) => task.team_id === id)
-    // const teamTaskComps = teamTasks.map((task) => <TaskCard key={task.id} task={task}/>)
     const teams = useSelector((state) => state.users.currentUser.teams)
     const team = teams.find((p) => p.id === id)
     const tasks = team ? team.tasks.map((t) => t) : []
@@ -19,15 +14,6 @@ function Team() {
     if (!team) {
         return <div>Please add your first task to this team.</div>
     }
-    // const uniqueProjects = new Set()
-    // team.projects.forEach((project) => {
-    //     uniqueProjects.add(project.name)
-    // })
-    // //need to create an array of uniq projects from the Team.projects
-    // const projects = Array.from(uniqueProjects).map((projectName) => {
-    //     const project = team.projects.find((project) => project.name === projectName)
-    //     return (<NavLink to={`/projects/${project.id}`} key={project.id}>{project.name}</NavLink>)
-    // })
 
     return(
         <div>   
