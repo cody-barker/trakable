b047608494619ac8c24695cd54a1a69d264d24a1
Cody <codybarker.or@gmail.com>
2023-09-20 11:05:45 -0700

    moving username to front of attrs

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :title, :email, :projects, :teams, :username
+  attributes :id, :username, :first_name, :last_name, :title, :email, :projects, :teams
   
   # has_many :tasks
   has_many :projects
--- client/src/teams/Teams.js
+++ client/src/teams/Teams.js
@@ -1,3 +1,36 @@
+// import { useSelector } from 'react-redux'
+// import { NavLink } from 'react-router-dom'
+// import { useState } from 'react'
+// import TeamForm from './TeamForm'
+
+// function Teams() {
+
+//     const currentUser = useSelector((state) => state.users.currentUser)
+//     const allTeams = useSelector((state) => state.teams.entities)
+    
+//     const userTeams = allTeams.filter((team) => team.creator_id === currentUser.id)
+//     const userTeamsComps = userTeams.map((team) => {
+//         return <NavLink to={`/teams/${team.id}`} key={team.id}>{team.name}</NavLink>
+//     })
+
+//     const [vis, setVis] = useState(false)
+//     function handleVis() {
+//         setVis(!vis)
+//     }
+
+//     return(
+//         <div>
+//             <button onClick={handleVis}>+ Add Team</button>
+//             {vis ? <TeamForm vis={vis} setVis={setVis}/> : null}
+//             <br></br>
+//             <h4>Teams I've Created</h4>
+//             {userTeamsComps}
+//         </div>
+//     )
+// }
+
+// export default Teams
+
 import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 import { useState } from 'react'
@@ -5,15 +38,14 @@ import TeamForm from './TeamForm'
 
 function Teams() {
 
+    const [vis, setVis] = useState(false)
     const currentUser = useSelector((state) => state.users.currentUser)
     const allTeams = useSelector((state) => state.teams.entities)
-    
-    const userTeams = allTeams.filter((team) => team.creator_id === currentUser.id)
+    const userTeams = allTeams.filter((team) => team.auth_users.includes(currentUser.id))
     const userTeamsComps = userTeams.map((team) => {
         return <NavLink to={`/teams/${team.id}`} key={team.id}>{team.name}</NavLink>
     })
 
-    const [vis, setVis] = useState(false)
     function handleVis() {
         setVis(!vis)
     }
@@ -23,7 +55,7 @@ function Teams() {
             <button onClick={handleVis}>+ Add Team</button>
             {vis ? <TeamForm vis={vis} setVis={setVis}/> : null}
             <br></br>
-            <h4>Teams I've Created</h4>
+            <h4>My Teams</h4>
             {userTeamsComps}
         </div>
     )
