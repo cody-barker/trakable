233b5d11798793c5f43a4db253d67f8b3b7c8a87
Cody <codybarker.or@gmail.com>
2023-09-25 09:41:45 -0700

    adding master key but ignoring git

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     resources :users, only: [:index]
     resources :tasks
     resources :projects
-  end
+  endgit 
 
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
