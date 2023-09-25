bacffbbaf5c35f68188003c8fa62087892e4e984
Cody <codybarker.or@gmail.com>
2023-09-25 11:28:16 -0700

    trying namespace instead of scope for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  scope :api do
+  namespace :api do
       post '/signup', to: 'users#create'
       get '/me', to: 'users#show'
       post '/login', to: 'sessions#create'
