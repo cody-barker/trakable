64fb4a8ae09e43921a9eb5d8410e8680d1508564
Cody <codybarker.or@gmail.com>
2023-10-02 12:05:24 -0700

    all teams comps refactored

--- client/src/teams/InviteForm.js
+++ client/src/teams/InviteForm.js
@@ -1,66 +1,75 @@
-import { useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { updateTeam } from './teamsSlice'
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateTeam } from './teamsSlice';
 
 function InviteForm() {
-    let { id } = useParams()
-    id = parseInt(id)
-    const dispatch = useDispatch()
-    const teams = useSelector((state) => state.teams.entities)
-    const errors = useSelector((state) => state.teams.errors)
-    const users = useSelector((state) => state.users.entities)
-    const [email, setEmail] = useState("")
-    const [emailNotFoundError, setEmailNotFoundError] = useState(false)
+  const { id } = useParams();
+  const teamId = parseInt(id);
+  const dispatch = useDispatch();
 
-    function handleChange(e) {
-        setEmail(e.target.value)
-        setEmailNotFoundError(false)
+  const teams = useSelector((state) => state.teams.entities);
+  const errors = useSelector((state) => state.teams.errors);
+  const users = useSelector((state) => state.users.entities);
+
+  const [email, setEmail] = useState('');
+  const [emailNotFoundError, setEmailNotFoundError] = useState(false);
+
+  const handleChange = (e) => {
+    setEmail(e.target.value);
+    setEmailNotFoundError(false);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const team = teams.find((t) => t.id === teamId);
+    const user = users.find((u) => u.email === email);
+
+    if (user) {
+      const updatedTeam = {
+        ...team,
+        auth_users: [...team.auth_users, user.id],
+      };
+
+      dispatch(updateTeam(updatedTeam));
+      setEmailNotFoundError(false);
+      setEmail('');
+    } else {
+      setEmailNotFoundError(true);
     }
+  };
 
-    function handleSubmit(e) {
-        e.preventDefault()
-        let team = teams.find((t) => t.id === id)
-        let user = users.find((u) => u.email === email)
-        
-        if (user) {
-            const updatedTeam = {
-                ...team,
-                auth_users: [...team.auth_users, user.id]
-            }
-            dispatch(updateTeam(updatedTeam))
-            setEmailNotFoundError(false)
-            setEmail("")
-        } else {
-            setEmailNotFoundError(true)
-        }
-    }
-
-    const errorComps = errors.map((teamErrors, teamIndex) => (
-        <div key={teamIndex}>
-            {teamErrors.errors.map((error, index) => (
-                <div className="errors" key={index}>{error.slice(11)}</div>
-            ))}
+  const errorComps = errors.map((teamErrors, teamIndex) => (
+    <div key={teamIndex}>
+      {teamErrors.errors.map((error, index) => (
+        <div className="errors" key={index}>
+          {error.slice(11)}
         </div>
-    ));
+      ))}
+    </div>
+  ));
 
-    return (
-        <form onSubmit={handleSubmit}>
-            {errorComps}
-            {emailNotFoundError && (
-                <div className="errors">User with this email not found.</div>
-            )}
-            <label>User's Email
-                <input
-                    onChange={handleChange}
-                    type="text"
-                    name="email"
-                    value={email}
-                />
-            </label>
-            <button className="btn" type="submit">Send Invite</button>
-        </form>
-    )
+  return (
+    <form onSubmit={handleSubmit}>
+      {errorComps}
+      {emailNotFoundError && (
+        <div className="errors">User with this email not found.</div>
+      )}
+      <label>
+        User's Email
+        <input
+          onChange={handleChange}
+          type="text"
+          name="email"
+          value={email}
+        />
+      </label>
+      <button className="btn" type="submit">
+        Send Invite
+      </button>
+    </form>
+  );
 }
 
-export default InviteForm
+export default InviteForm;
