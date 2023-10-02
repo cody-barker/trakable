8b1ccb4d77c3bfbeed612d356dfe5f92f82559e4
Cody <codybarker.or@gmail.com>
2023-10-02 11:34:09 -0700

    teamsSlice changed to builder cases:

--- client/src/teams/teamsSlice.js
+++ client/src/teams/teamsSlice.js
@@ -32,55 +32,53 @@ export const updateTeam = createAsyncThunk("teams/updateTeam", (payload) => {
 const teamsSlice = createSlice({
     name: "teams",
     initialState: {
-        status: "idle",
-        entities: [],
-        errors: []
+      status: "idle",
+      entities: [],
+      errors: [],
     },
-    reducers: {
-    },
-    extraReducers: {
-        //fetchteams
-        [fetchTeams.pending](state) {
-            state.status = "loading";
-        },
-        [fetchTeams.fulfilled](state, action) {
-            state.entities = action.payload;
-            state.status = "idle";
-        },
-        //createTeam
-        [createTeam.pending](state) {
-            state.status = "loading";
-        },
-        [createTeam.fulfilled](state, action) {
-            state.status = "idle";
-            if ('errors' in action.payload) {
-                state.errors = [];
-                state.errors.push(action.payload)
-            } else {
-                state.entities.push(action.payload);
-                state.errors = [];
+    reducers: {},
+    extraReducers: (builder) => {
+      builder
+        .addCase(fetchTeams.pending, (state) => {
+          state.status = "loading";
+        })
+        .addCase(fetchTeams.fulfilled, (state, action) => {
+          state.entities = action.payload;
+          state.status = "idle";
+        })
+        .addCase(createTeam.pending, (state) => {
+          state.status = "loading";
+        })
+        .addCase(createTeam.fulfilled, (state, action) => {
+          state.status = "idle";
+          if ("errors" in action.payload) {
+            state.errors = [];
+            state.errors.push(action.payload);
+          } else {
+            state.entities.push(action.payload);
+            state.errors = [];
+          }
+        })
+        .addCase(updateTeam.pending, (state) => {
+          state.status = "loading";
+        })
+        .addCase(updateTeam.fulfilled, (state, action) => {
+          state.status = "idle";
+          if ("errors" in action.payload) {
+            state.errors = [];
+            state.errors.push(action.payload);
+          } else {
+            state.entities = state.entities.map((t) => {
+              if (t.id === action.payload.id) {
+                return action.payload;
+              } else {
+                return t;
+              }
+            });
+            state.errors = [];
             }
         },
-        //updateTeam
-        [updateTeam.pending](state) {
-            state.status = "loading"
-        },
-        [updateTeam.fulfilled](state, action) {
-            state.status = "idle"
-            if ('errors' in action.payload) {
-                state.errors = [];
-                state.errors.push(action.payload)
-            } else {
-                state.entities = state.entities.map((t) => {
-                    if (t.id === action.payload.id) {
-                        return action.payload
-                    } else {
-                        return t
-                    }
-                })
-                state.errors = [];
-            }
-        },
+        )
     }
 })
 
