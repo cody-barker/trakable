bc2ca72844976b3c0f82b3d88765ea2bf64acdb2
Cody <codybarker.or@gmail.com>
2023-10-02 12:13:55 -0700

    TeamTaskCard refactored

--- client/src/tasks/TeamTaskCard.js
+++ client/src/tasks/TeamTaskCard.js
@@ -1,12 +1,12 @@
-import {NavLink, useNavigate} from 'react-router-dom'
-import {useDispatch} from 'react-redux'
-import {deleteTask} from '../users/usersSlice'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import { deleteTask } from '../users/usersSlice'
 import { useSelector } from 'react-redux'
 
-function TeamTaskCard({task}) {
+function TeamTaskCard({ task }) {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
 
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
     const {
         name,
         due_date,
@@ -14,7 +14,7 @@ function TeamTaskCard({task}) {
         project_id,
         team_id,
         user_id
-    } = task
+    } = task;
 
     //limit characters for task name
     const maxCharacters = 40;
@@ -23,11 +23,14 @@ function TeamTaskCard({task}) {
     const users = useSelector((state) => state.users.entities)
     const currentUser = useSelector((state) => state.users.currentUser)
     const user = users.find((u) => u.id === user_id)
+
     if (!user) {
         return <tr><td>"Loading..."</td></tr>
     }
+
     const project = user.projects.find((p) => p.id === project_id)
     const team = user.teams.find((t) => t.id === team_id)
+
     function handleComplete() {
         dispatch(deleteTask(id))
     }
@@ -36,52 +39,72 @@ function TeamTaskCard({task}) {
         navigate(`/tasks/${id}/edit`)
     }
 
-    const today = new Date();
-    const yesterday = new Date();
-    yesterday.setDate(yesterday.getDate() - 1); 
+    const getDueDateTextAndColor = () => {
+        const today = new Date();
+        const yesterday = new Date();
+        yesterday.setDate(yesterday.getDate() - 1);
 
-    // Format today's date
-    const yyyyToday = today.getFullYear();
-    let mmToday = today.getMonth() + 1;
-    let ddToday = today.getDate();
+        const formattedToday = formatDate(today);
+        const formattedYesterday = formatDate(yesterday);
 
-    if (ddToday < 10) ddToday = '0' + ddToday;
-    if (mmToday < 10) mmToday = '0' + mmToday;
-    const formattedToday = yyyyToday + "-" + mmToday + "-" + ddToday;
+        if (formattedToday === due_date) {
+            return { text: 'Today', color: 'green' };
+        } else if (formattedYesterday === due_date) {
+            return { text: 'Yesterday', color: 'red' };
+        } else if (new Date(due_date) < today) {
+            return { text: due_date, color: 'red' };
+        }
 
-    // Format yesterday's date
-    const yyyyYesterday = yesterday.getFullYear();
-    let mmYesterday = yesterday.getMonth() + 1;
-    let ddYesterday = yesterday.getDate();
+        return { text: due_date, color: '' };
+    };
 
-    if (ddYesterday < 10) ddYesterday = '0' + ddYesterday;
-    if (mmYesterday < 10) mmYesterday = '0' + mmYesterday;
-    const formattedYesterday = yyyyYesterday + "-" + mmYesterday + "-" + ddYesterday;
+    const formatDate = (date) => {
+        const yyyy = date.getFullYear();
+        let mm = date.getMonth() + 1;
+        let dd = date.getDate();
 
-    let dueDateText = due_date;
-    let dueDateColor = ''; // Empty string for default color
+        if (dd < 10) dd = '0' + dd;
+        if (mm < 10) mm = '0' + mm;
 
-    if (formattedToday === due_date) {
-        dueDateText = 'Today';
-        dueDateColor = 'green'; // Apply green color for today
-    } else if (formattedYesterday === due_date) {
-        dueDateText = 'Yesterday';
-        dueDateColor = 'red'; // Apply red color for yesterday
-    } else if (new Date(due_date) < today) {
-        dueDateColor = 'red'; // Apply red color for dates earlier than today
-    }
-    
-    return(
+        return `${yyyy}-${mm}-${dd}`;
+    };
+
+    const { text: dueDateText, color: dueDateColor } = getDueDateTextAndColor();
+
+    return (
         <tr className="table-row">
-            <td>{currentUser.id === user_id ? <button className="icon-container" onClick={handleComplete}><img className="checkbox-icon" src="https://cdns.iconmonstr.com/wp-content/releases/preview/2018/240/iconmonstr-check-mark-circle-thin.png"/></button> : null}</td>
-            {/* <td>{currentUser.id === user.id ? <NavLink className="task-card" to={`/tasks/${id}`}>{truncatedName}</NavLink> : truncatedName}</td> */}
-            <td><NavLink className="task-card" to={`/tasks/${id}`}>{truncatedName}</NavLink></td>
-            <td><span style={{ color: dueDateColor }}>{dueDateText}</span></td>
+            <td>
+                {currentUser.id === user_id ? (
+                <button className="icon-container" onClick={handleComplete}>
+                    <img className="checkbox-icon" src="https://cdns.iconmonstr.com/wp-content/releases/preview/2018/240/iconmonstr-check-mark-circle-thin.png" />
+                </button>
+                ) : null}
+            </td>
+            <td>
+                <NavLink className="task-card" to={`/tasks/${id}`}>
+                {truncatedName}
+                </NavLink>
+            </td>
+            <td>
+                <span style={{ color: dueDateColor }}>{dueDateText}</span>
+            </td>
             <td>{user.username}</td>
-            <td>{currentUser.id === user_id ? <NavLink className="task-card" to={`/projects/${project.id}`}>{project.name}</NavLink> : project.name}</td>
-            <td>{currentUser.id === user_id ? <button className="icon-container" onClick={handleEdit}><img className="edit-icon" src="https://cdns.iconmonstr.com/wp-content/releases/preview/7.8.0/240/iconmonstr-pencil-text-lined.png"/></button> : null}</td>
+            <td>
+                {currentUser.id === user_id ? (
+                <NavLink className="task-card" to={`/projects/${project.id}`}>
+                    {project.name}
+                </NavLink>
+                ) : project.name}
+            </td>
+            <td>
+                {currentUser.id === user_id ? (
+                <button className="icon-container" onClick={handleEdit}>
+                    <img className="edit-icon" src="https://cdns.iconmonstr.com/wp-content/releases/preview/7.8.0/240/iconmonstr-pencil-text-lined.png" />
+                </button>
+                ) : null}
+            </td>
         </tr>
-    )
+    );
 }
 
-export default TeamTaskCard
+export default TeamTaskCard;
