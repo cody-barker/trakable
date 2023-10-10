c4c67de24214c88792f89b99d8e2061ffa3052b9
Cody <codybarker.or@gmail.com>
2023-10-10 10:14:52 -0700

    moving get path route in routes.rb to bottom so I have access to api in development

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
       post '/signup', to: 'users#create'
       get '/me', to: 'users#show'
       post '/login', to: 'sessions#create'
@@ -10,4 +9,7 @@ Rails.application.routes.draw do
       resources :users, only: [:index]
       resources :tasks, except: [:show]
       resources :projects, only: [:index, :create]
+
+      get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
 end
