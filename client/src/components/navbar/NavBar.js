29c44f58502c1f09a3fd8e9520246db2f942110b
Cody <codybarker.or@gmail.com>
2024-01-16 14:36:03 -0800

    add navbar primary and secondary container divs to navbar

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -1,26 +1,35 @@
-import { NavLink, useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { logoutUser } from '../../state/usersSlice'
-
+import { NavLink, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "../../state/usersSlice";
 
 function NavBar() {
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
-    function handleLogout() {
-        dispatch(logoutUser())
-        navigate("/")
-    }
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  function handleLogout() {
+    dispatch(logoutUser());
+    navigate("/");
+  }
 
-    return(
-        <nav id="navbar">
-            <NavLink className="nav-btn orange" to="/">Tasks</NavLink>
-            <NavLink className="nav-btn green" to={`/projects`}>Projects</NavLink>
-            <NavLink className="nav-btn yellow" to={`/teams`}>Teams</NavLink>
-            <button className="nav-btn blue right" onClick={handleLogout}>
-                Logout
-            </button>
-        </nav>
-    )
+  return (
+    <nav id="navbar">
+      <div className="navbar__primary">
+        <NavLink className="nav-btn orange" to="/">
+          Tasks
+        </NavLink>
+        <NavLink className="nav-btn green" to={`/projects`}>
+          Projects
+        </NavLink>
+        <NavLink className="nav-btn yellow" to={`/teams`}>
+          Teams
+        </NavLink>
+      </div>
+      <div className ="navbar__secondary">
+        <button className="nav-btn blue right" onClick={handleLogout}>
+            Logout
+        </button>
+      </div>
+    </nav>
+  );
 }
 
-export default NavBar
+export default NavBar;
